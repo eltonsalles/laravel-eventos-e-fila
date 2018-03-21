cc3522206488d9e4f22d533a10a6758a96651fad
Elton Rodrigues <eltonrms.leite@hotmail.com>
2018-03-21 15:23:57 -0300

    criado novos eventos para a model user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,35 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Antes de salvar no banco de dados
+        User::creating(function ($user) {
+            \Log::info("Usuário {$user->email} vai ser criado");
+        });
+
+        // Após salvar no banco de dados
         User::created(function ($user) {
-            \Mail::to($user)->send(new UserRegistered($user));
+//            \Mail::to($user)->send(new UserRegistered($user));
+            \Log::info("Usuário {$user->email} foi criado");
+        });
+
+        // Antes de alterar no banco de dados
+        User::updating(function ($user) {
+            \Log::info("Usuário {$user->email} vai ser alterado");
+        });
+
+        // Após alterar no banco de dados
+        User::updated(function ($user) {
+            \Log::info("Usuário {$user->email} foi alterado");
+        });
+
+        // Antes de salvar ou alterar no banco de dados
+        User::saving(function ($user) {
+            \Log::info("Usuário {$user->email} vai ser salvo");
+        });
+
+        // Após salvar ou alterar no banco de dados
+        User::saved(function ($user) {
+            \Log::info("Usuário {$user->email} foi salvo");
         });
     }
 
